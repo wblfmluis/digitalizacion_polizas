062319e7e69c56aa3ef5e88ca48b124e27e4c4a6
wblfmluis <luis@wiboo.com.mx>
2026-04-04 20:48:43 -0600

    Fix Redis Conn

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -8,6 +8,7 @@ import { BullModule } from '@nestjs/bullmq';
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT ?? 6379),
         password: process.env.REDIS_PASSWORD,
+        maxRetriesPerRequest: null,
       },
     }),
     BullModule.registerQueue({
